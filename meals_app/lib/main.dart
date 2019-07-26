4aea3164d1070ad852c3e5a14e3f59b6ee11e23e
xxGus <gustavo_baptista@live.com>
2019-07-26 13:23:11 -0300

    Added a TabBar to the Appbar

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 
+import './screens/tabs_screen.dart';
 import './screens/category_meals_screen.dart';
-import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,8 +33,9 @@ class MyApp extends StatelessWidget {
       ),
       // home: CategoriesScreen(),
       routes: {
-        '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
+        '/': (ctx) => TabsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
       },
     );
   }
